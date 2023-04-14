cf0274a2f42959371b63173726b1b02d2234e42d
ritchie.octo <ritchie.elloumi@octo.com>
2023-04-14 11:01:19 +0200

    tech: remove unused import

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,10 @@
-import { Controller, Get, Redirect } from '@nestjs/common'
+import { Controller, Get } from '@nestjs/common'
 import { ApiExcludeController } from '@nestjs/swagger'
 
 @Controller('')
 @ApiExcludeController()
 export class RedirectController {
   @Get()
-  // TO DO : décommenter lorsque le swagger est initié
-  // @Redirect('/doc')
   redirect(): string {
     return null
   }
