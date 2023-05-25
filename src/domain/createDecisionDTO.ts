562230c65094e4b843f6156cf2c60434cee30264
Fabricio ALEGRIA <f.alegria.herrera@octo.com>
2023-05-25 15:49:43 +0200

    doc(327): add swagger on createDecisionDTO

--- src/domain/createDecisionDTO.ts
+++ src/domain/createDecisionDTO.ts
@@ -11,45 +11,108 @@ import {
 } from 'class-validator'
 import { DecisionStatus } from './enum'
 import { Type } from 'class-transformer'
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger'
+import { MockUtils } from 'src/infrastructure/utils/mock.utils'
 
+const mockUtils = new MockUtils()
 export class DecisionOccultation {
+  @ApiProperty({
+    description: "Termes additionnels à ajouter lors de l'occultation",
+    type: String,
+    example: new MockUtils().createDecisionDTO.occultation.additionalTerms
+  })
   @IsString()
   additionalTerms: string
 
+  @ApiProperty({
+    description: "Catégories additionnels à omettre lors de l'occultation",
+    type: [String],
+    example: new MockUtils().createDecisionDTO.occultation.categoriesToOmit
+  })
   @IsString({ each: true })
   categoriesToOmit: string[]
 }
 
 export class DecisionAnalyse {
+  @ApiProperty({
+    description: "Eléments de titrage et d'analyse complémentaires",
+    type: [String],
+    example: mockUtils.createDecisionDTO.analysis.analyse
+  })
   @IsString({ each: true })
   analyse: string[]
 
+  @ApiProperty({
+    description: 'Doctrine',
+    type: String,
+    example: mockUtils.createDecisionDTO.analysis.doctrine
+  })
   @IsString()
   doctrine: string
 
+  @ApiProperty({
+    description: 'Rapprochements de jurisprudence',
+    type: String,
+    example: mockUtils.createDecisionDTO.analysis.link
+  })
   @IsString()
   link: string
 
+  @ApiProperty({
+    description: "Eléments de titrage et d'analyse complémentaires",
+    type: Array,
+    example: mockUtils.createDecisionDTO.analysis.reference
+  })
   @IsString({ each: true })
   reference: string[]
 
+  @ApiProperty({
+    description: 'Source',
+    type: String,
+    example: mockUtils.createDecisionDTO.analysis.source
+  })
   @IsString()
   source: string
 
+  @ApiProperty({
+    description: 'Résumé de la décision',
+    type: String,
+    example: mockUtils.createDecisionDTO.analysis.summary
+  })
   @IsString()
   summary: string
 
+  @ApiProperty({
+    description: 'Texte(s) visé(s)',
+    type: String,
+    example: mockUtils.createDecisionDTO.analysis.target
+  })
   @IsString()
   target: string
 
+  @ApiProperty({
+    description: 'Eléments de titrage',
+    type: [String],
+    example: mockUtils.createDecisionDTO.analysis.title
+  })
   @IsString({ each: true })
   title: string[]
 }
 
 export class CreateDecisionDTO {
+  @ApiProperty({
+    description: 'Identifiant de la decision',
+    type: String,
+    example: mockUtils.createDecisionDTO.id
+  })
   @IsString()
   id: string
 
+  @ApiProperty({
+    description: 'Analyse de la décision',
+    type: () => DecisionAnalyse,
+    example: mockUtils.createDecisionDTO.analysis
+  })
   @IsDefined()
   @IsObject()
   @IsNotEmptyObject()
@@ -57,39 +120,101 @@ export class CreateDecisionDTO {
   @Type(() => DecisionAnalyse)
   analysis: DecisionAnalyse
 
+  @ApiProperty({
+    description: 'Numéro(s) de pourvoi de la décision',
+    type: [String],
+    example: mockUtils.createDecisionDTO.appeals
+  })
   @IsString({ each: true })
   appeals: string[]
 
+  @ApiProperty({
+    description: 'Composante de chamberName afin de pouvoir etre lisible pour les agents',
+    type: String,
+    example: mockUtils.createDecisionDTO.chamberId
+  })
   @IsString()
   chamberId: string
 
+  @ApiProperty({
+    description:
+      "Nom de la chambre à la Cour de cassation, en fonction du niveau de l'affaire ou du thème : 'Chambre sociale','Chambre commerciale',...",
+    type: String,
+    example: mockUtils.createDecisionDTO.chamberName
+  })
   @IsString()
   chamberName: string
 
+  @ApiProperty({
+    description: 'Identifiant technique de la décision',
+    type: String,
+    example: mockUtils.createDecisionDTO.chamberName
+  })
   @IsString()
   iddecision: string
 
+  @ApiProperty({
+    description: 'Date de la création (après réception) de la décision',
+    type: String,
+    example: mockUtils.createDecisionDTO.dateCreation
+  })
   @IsString()
   dateCreation: string
 
+  @ApiProperty({
+    description: 'Date de la décision',
+    type: String,
+    example: mockUtils.createDecisionDTO.dateDecision
+  })
   @IsString()
   dateDecision: string
 
+  @ApiProperty({
+    description: 'Décision(s) attaquée(s)',
+    type: [Number],
+    example: mockUtils.createDecisionDTO.decatt
+  })
   @IsNumber({}, { each: true })
   decatt: number[]
 
+  @ApiProperty({
+    description: 'Manière de codifier et/ou identifier la juridiction émettrice',
+    type: String,
+    example: mockUtils.createDecisionDTO.jurisdictionCode
+  })
   @IsString()
   jurisdictionCode: string
 
+  @ApiProperty({
+    description: 'Manière de codifier et/ou identifier la juridiction émettrice',
+    type: String,
+    example: mockUtils.createDecisionDTO.jurisdictionId
+  })
   @IsString()
   jurisdictionId: string
 
+  @ApiProperty({
+    description:
+      "le titre de la juridiction. Ex : 'Cour d'appel d'Angers', 'Tribunal judiciaire de Bobigny'",
+    type: String,
+    example: mockUtils.createDecisionDTO.jurisdictionName
+  })
   @IsString()
   jurisdictionName: string
 
+  @ApiProperty({
+    description: 'Statut de la décision dans Judilibre',
+    enum: DecisionStatus,
+    example: mockUtils.createDecisionDTO.labelStatus
+  })
   @IsEnum(DecisionStatus)
   labelStatus: DecisionStatus
 
+  @ApiProperty({
+    description: 'Statut de la décision dans Judilibre',
+    type: DecisionOccultation,
+    example: mockUtils.createDecisionDTO.occultation
+  })
   @ValidateNested()
   @Type(() => DecisionOccultation)
   occultation: {
@@ -97,43 +222,103 @@ export class CreateDecisionDTO {
     categoriesToOmit: string[]
   }
 
+  @ApiProperty({
+    description: 'Le texte original de la décision',
+    type: String,
+    example: mockUtils.createDecisionDTO.originalText
+  })
   @IsString()
   originalText: string
 
+  @ApiPropertyOptional({
+    description: "(interne) L'état de la pseudonymisation",
+    type: String
+  })
   @IsString()
   @IsOptional()
   pseudoStatus?: string
 
+  @ApiPropertyOptional({
+    description: 'Le texte de la décision pseudonymisé',
+    type: String
+  })
   @IsString()
   @IsOptional()
   pseudoText?: string
 
+  @ApiPropertyOptional({
+    description: 'La décision est-elle publique ?',
+    type: Boolean
+  })
   @IsBoolean()
   @IsOptional()
   public?: boolean
 
+  @ApiProperty({
+    description: 'Numéro de registre',
+    type: Boolean,
+    example: mockUtils.createDecisionDTO.registerNumber
+  })
   @IsString()
   registerNumber: string
 
+  @ApiProperty({
+    description:
+      "Circuit de relecture pour jurinet ex: 'Avis sur saisine', 'Non-admission', 'Déchéance',",
+    type: String,
+    example: mockUtils.createDecisionDTO.solution
+  })
   @IsString()
   solution: string
 
+  @ApiProperty({
+    description: 'ID de la décision (ID Oracle)',
+    type: Number,
+    example: mockUtils.createDecisionDTO.sourceId
+  })
   @IsNumber()
   sourceId: number
 
+  @ApiProperty({
+    description: "Nom de la source ex : 'jurica', 'jurinet', 'juritj'",
+    type: String,
+    example: mockUtils.createDecisionDTO.sourceName
+  })
   @IsString()
   sourceName: string
 
+  @ApiPropertyOptional({
+    description: '(interne) Mise en page des décisions diffusées par extrait',
+    type: String,
+    example: mockUtils.createDecisionDTO.sourceName
+  })
   @IsObject()
   @IsOptional()
   zoning?: object
 
+  @ApiProperty({
+    description:
+      'La propriété publication est alimenté par les nouveaux indicateurs positionnés en amont: IND_BULLETIN, IND_RAPPORT, IND_LETTRE et IND_COMMUNIQUE (CC only)',
+    type: [String],
+    example: mockUtils.createDecisionDTO.publication
+  })
   @IsString({ each: true })
   publication: string[]
 
+  @ApiProperty({
+    description:
+      ' Champ session dans Label :circuit de relecture, pour la Cour de cassation ex: FRR -> formation restreinte',
+    type: [String],
+    example: mockUtils.createDecisionDTO.formation
+  })
   @IsString()
   formation: string
 
+  @ApiProperty({
+    description: "Numero du bloc d'occultation",
+    type: Number,
+    example: mockUtils.createDecisionDTO.blocOccultation
+  })
   @IsNumber()
   blocOccultation: number
 
--- src/infrastructure/controllers/decisions.controller.ts
+++ src/infrastructure/controllers/decisions.controller.ts
@@ -15,9 +15,11 @@ import {
 import {
   ApiAcceptedResponse,
   ApiBadRequestResponse,
+  ApiBody,
   ApiHeader,
   ApiQuery,
-  ApiTags
+  ApiTags,
+  ApiUnauthorizedResponse
 } from '@nestjs/swagger'
 import { MockUtils } from '../utils/mock.utils'
 import { GetDecisionListDTO } from '../../domain/getDecisionList.dto'
@@ -45,6 +47,9 @@ export class DecisionsController {
   @ApiBadRequestResponse({
     description: "Le paramètre  écrit n'est présent dans la liste des valeurs acceptées"
   })
+  @ApiUnauthorizedResponse({
+    description: "Vous n'avez pas accès à cette route"
+  })
   getDecisions(
     @Query(
       'status',
@@ -66,26 +71,31 @@ export class DecisionsController {
     name: 'x-api-key',
     description: 'Clé API'
   })
-  @ApiQuery({
-    name: 'decision',
-    description: 'Décision intègre au format wordperfect et metadonnées associées.'
+  @ApiBody({
+    description: 'Décision intègre au format wordperfect et metadonnées associées.',
+    type: CreateDecisionDTO
   })
   @ApiAcceptedResponse({ description: 'Decision créée' })
   @ApiBadRequestResponse({
-    description: 'Il manque un ou plusieurs champs obligatoires pour la décision'
+    description: 'Il manque un ou plusieurs champs obligatoires dans la décision'
+  })
+  @ApiUnauthorizedResponse({
+    description: "Vous n'avez pas accès à cette route"
   })
   @UsePipes()
   createDecisions(
     @Request() req,
     @Body('decision', new ValidateDtoPipe()) decision: CreateDecisionDTO
   ) {
+    const authorizedKeys = [process.env.NORMALIZATION_API_KEY, process.env.OPENSDER_API_KEY]
     const apiKey = req.headers['x-api-key']
-    if (apiKey !== process.env.NORMALIZATION_API_KEY) {
+    if (!authorizedKeys.includes(apiKey)) {
       throw new ForbiddenException()
     }
 
     const createDecisionUsecase = new CreateDecisionUsecase()
     createDecisionUsecase.execute(decision)
+    this.logger.log('POST /decisions called with ' + JSON.stringify(decision))
     return {
       message: 'Decision créée'
     }
