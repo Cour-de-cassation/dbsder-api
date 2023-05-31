e179785b4f2fb644f3a1f36cfa8c2a3a989193d4
Fabricio ALEGRIA <f.alegria.herrera@octo.com>
2023-05-31 17:27:08 +0200

    tech(327): split controller tests by feature (create, getList)

---
+++ src/infrastructure/controllers/getDecisionsList.controller.ispec.ts
@@ -0,0 +1,118 @@
+import * as request from 'supertest'
+import { HttpStatus, INestApplication } from '@nestjs/common'
+import { Test, TestingModule } from '@nestjs/testing'
+import { AppModule } from '../../app.module'
+import { MockUtils } from '../utils/mock.utils'
+import { DecisionStatus } from '../../domain/enum'
+import mongoose from 'mongoose'
+import { MongoMemoryServer } from 'mongodb-memory-server'
+
+describe('DecisionsController', () => {
+  let app: INestApplication
+  const mockUtils = new MockUtils()
+  const labelApiKey = process.env.LABEL_API_KEY
+  let mongoMemoryServer: MongoMemoryServer
+  // A changer aussi dans .jest/setupEnvVars.ts
+  const mongoServerMemoryPort = 5353
+
+  beforeAll(async () => {
+    mongoMemoryServer = await MongoMemoryServer.create({
+      instance: {
+        port: mongoServerMemoryPort
+      }
+    })
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule]
+    }).compile()
+
+    app = moduleFixture.createNestApplication()
+
+    await app.init()
+  })
+
+  afterAll(async () => {
+    await mongoMemoryServer.stop()
+    await mongoose.disconnect()
+  })
+
+  describe('GET /decisions', () => {
+    describe('returns 401 Unauthorized', () => {
+      it('when apiKey is not provided', async () => {
+        // WHEN
+        const result = await request(app.getHttpServer()).get('/decisions')
+
+        // THEN
+        expect(result.statusCode).toEqual(HttpStatus.UNAUTHORIZED)
+      })
+
+      it('when apiKey does not exist', async () => {
+        // WHEN
+        const unknownApiKey = 'notTestLabelApiKey'
+        const result = await request(app.getHttpServer())
+          .get('/decisions')
+          .set({ 'x-api-key': unknownApiKey })
+
+        // THEN
+        expect(result.statusCode).toEqual(HttpStatus.UNAUTHORIZED)
+      })
+    })
+
+    describe('returns 400 Bad Request error', () => {
+      it('when status is missing', async () => {
+        // WHEN
+        const result = await request(app.getHttpServer())
+          .get('/decisions')
+          .set({ 'x-api-key': labelApiKey })
+
+        // THEN
+        expect(result.statusCode).toEqual(HttpStatus.BAD_REQUEST)
+      })
+
+      it('when status does not exist', async () => {
+        // GIVEN
+        const statusNotAccepted = 'tata'
+
+        // WHEN
+        const result = await request(app.getHttpServer())
+          .get('/decisions')
+          .query({ status: statusNotAccepted })
+          .set({ 'x-api-key': labelApiKey })
+
+        // THEN
+        expect(result.statusCode).toEqual(HttpStatus.BAD_REQUEST)
+      })
+    })
+
+    describe('returns 403 Forbidden', () => {
+      it('when the apiKey is not authorized to call this endpoint', async () => {
+        // GIVEN
+        const normalisationApiKey = process.env.NORMALIZATION_API_KEY
+
+        // WHEN
+        const result = await request(app.getHttpServer())
+          .get('/decisions')
+          .query({ status: DecisionStatus.TOBETREATED })
+          .set({ 'x-api-key': normalisationApiKey })
+
+        // THEN
+
+        expect(result.statusCode).toEqual(HttpStatus.FORBIDDEN)
+      })
+    })
+
+    it('returns a 200 with a list of decisions', async () => {
+      // GIVEN
+      const expectedDecisions = mockUtils.allDecisionsToBeTreated
+
+      // WHEN
+      const result = await request(app.getHttpServer())
+        .get('/decisions')
+        .query({ status: DecisionStatus.TOBETREATED })
+        .set({ 'x-api-key': labelApiKey })
+
+      // THEN
+      expect(result.statusCode).toEqual(HttpStatus.OK)
+      expect(result.body).toEqual(expectedDecisions)
+    })
+  })
+})
