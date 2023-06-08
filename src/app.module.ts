2369ef9fc6b4235b2e97a81e096b82a6cbea64e3
Armen Ozcelik <armen.ozcelik@octo.com>
2023-06-08 17:21:37 +0200

    tech: link database to nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,22 @@
 import { ConfigModule } from '@nestjs/config'
+import { MongooseModule } from '@nestjs/mongoose'
 import { MiddlewareConsumer, Module } from '@nestjs/common'
+import { RedirectController } from './app.controller'
 import { AuthModule } from './infrastructure/auth/auth.module'
+import { DecisionSchema } from './infrastructure/db/models/decision.model'
 import { AuthMiddleware } from './infrastructure/middleware/auth/auth.middleware'
 import { DecisionsController } from './infrastructure/controllers/decisions.controller'
-import { RedirectController } from './app.controller'
+import { MongoRepository } from './infrastructure/db/repositories/mongo.repository'
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule],
+  imports: [
+    ConfigModule.forRoot(),
+    AuthModule,
+    MongooseModule.forRoot(process.env.MONGO_DB_URL),
+    MongooseModule.forFeature([{ name: 'DecisionModel', schema: DecisionSchema }])
+  ],
   controllers: [RedirectController, DecisionsController],
-  providers: []
+  providers: [MongoRepository]
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
--- src/infrastructure/db/repositories/mongo.repository.ts
+++ src/infrastructure/db/repositories/mongo.repository.ts
@@ -1,33 +1,17 @@
-import mongoose, { Mongoose } from 'mongoose'
+import { Model } from 'mongoose'
+import { InjectModel } from '@nestjs/mongoose'
+import { ServiceUnavailableException } from '@nestjs/common'
+import { DecisionModel } from '../models/decision.model'
 import { CreateDecisionDTO } from '../../createDecisionDTO'
 import { IDatabaseRepository } from '../../../domain/database.repository.interface'
-import { DecisionModel, DecisionSchema } from '../models/decision.model'
-import { ServiceUnavailableException } from '@nestjs/common'
 
 export class MongoRepository implements IDatabaseRepository {
-  private mongoClient: Mongoose
-
-  constructor(private readonly mongoURL: string, mongoClient?: Mongoose) {
-    if (mongoClient) {
-      this.mongoClient = mongoClient
-    }
-  }
+  constructor(@InjectModel('DecisionModel') private decisionModel: Model<DecisionModel>) {}
 
   async create(decision: CreateDecisionDTO): Promise<DecisionModel> {
-    await this.setMongoClient()
-    if (this.mongoClient.model) {
-      const collection = this.mongoClient.model('decisions', DecisionSchema)
-      const savedDecision = await collection.create(decision).catch(() => {
-        throw new ServiceUnavailableException('Error from database')
-      })
-      return Promise.resolve(savedDecision)
-    }
-    return null
-  }
-
-  async setMongoClient() {
-    if (!this.mongoClient) {
-      this.mongoClient = await mongoose.connect(this.mongoURL)
-    }
+    const savedDecision = await this.decisionModel.create(decision).catch(() => {
+      throw new ServiceUnavailableException('Error from database')
+    })
+    return Promise.resolve(savedDecision)
   }
 }
