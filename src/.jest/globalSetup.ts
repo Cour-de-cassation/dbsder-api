f041fbb8d3141a8868a561a69a5016dc7d4b97f7
Armen Ozcelik <armen.ozcelik@free.fr>
2023-06-12 20:48:10 +0200

    WIP

--- src/.jest/globalSetup.ts
+++ src/.jest/globalSetup.ts
@@ -4,8 +4,7 @@ import { mongoDbMemoryServerConf } from './mongoDbMemoryServer.conf'
 
 export default async function globalSetup() {
   if (mongoDbMemoryServerConf.Memory) {
-    // Config to decided if an mongodb-memory-server instance should be used
-    // it's needed in global space, because we don't want to create a new instance every test-suite
+    // To reuse database in test suite
     const instance = await MongoMemoryServer.create({
       instance: {
         port: parseInt(mongoDbMemoryServerConf.Port),
@@ -20,7 +19,7 @@ export default async function globalSetup() {
     process.env.MONGO_URI = `mongodb://${mongoDbMemoryServerConf.IP}:${mongoDbMemoryServerConf.Port}`
   }
 
-  // The following is to make sure the database is clean before an test starts
+  // Cleaning before test starts
   await mongoose.connect(`${process.env.MONGO_URI}/${mongoDbMemoryServerConf.Database}`)
   await mongoose.connection.db.dropDatabase()
   await mongoose.disconnect()
--- src/.jest/globalTearDown.ts
+++ src/.jest/globalTearDown.ts
@@ -3,7 +3,6 @@ import { mongoDbMemoryServerConf } from './mongoDbMemoryServer.conf'
 
 export default async function globalTeardown() {
   if (mongoDbMemoryServerConf.Memory) {
-    // Config to decided if an mongodb-memory-server instance should be used
     const instance: MongoMemoryServer = (global as any).__MONGOINSTANCE
     await instance.stop()
   }
