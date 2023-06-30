19110c9d1eb4a15f2e4ca060e4820d88ff3b597a
ritchie.octo <ritchie.elloumi@octo.com>
2023-06-30 09:56:44 +0200

    wip: rename convict schema

--- src/convict-config.ts
+++ src/convict-config.ts
@@ -1,37 +1,37 @@
 import * as convict from 'convict'
 
 export const convict_config = convict({
-  docLoginSwagger: {
-    doc: 'Login for Swagger documentation',
-    format: String,
-    default: '',
-    env: process.env.DOC_LOGIN
-  },
-  docPasswordSwagger: {
-    doc: 'Password for Swagger documentation',
-    format: String,
-    default: '',
-    env: process.env.DOC_PASSWORD
-  },
-  labelApiKey: {
+  LABEL_API_KEY: {
     doc: 'Label API Key',
     format: String,
     default: '',
     env: process.env.LABEL_API_KEY
   },
-  normalizationApiKey: {
+  NORMALIZATION_API_KEY: {
     doc: 'Normalization API Key',
     format: String,
     default: '',
     env: process.env.NORMALIZATION_API_KEY
   },
-  openSderApiKey: {
+  OPENSDER_API_KEY: {
     doc: 'Open SDER API Key',
     format: String,
     default: '',
     env: process.env.OPENSDER_API_KEY
   },
-  mongoDBUrl: {
+  DOC_LOGIN: {
+    doc: 'Login for Swagger documentation',
+    format: String,
+    default: '',
+    env: process.env.DOC_LOGIN
+  },
+  DOC_PASSWORD: {
+    doc: 'Password for Swagger documentation',
+    format: String,
+    default: '',
+    env: process.env.DOC_PASSWORD
+  },
+  MONGO_DB_URL: {
     doc: 'Mongo DB Url',
     format: String,
     default: '',
