ee9e89ffa33627af2d6f941bf54ea3eabe0843a2
Armen Ozcelik <armen.ozcelik@octo.com>
2023-07-04 17:10:26 +0200

    tech: add DOC_LOGIN and DOC_PASSWORD variables for tests suites

--- src/.jest/setupEnvVars.ts
+++ src/.jest/setupEnvVars.ts
@@ -3,5 +3,7 @@ process.env = {
   NORMALIZATION_API_KEY: '9ad44ab5-3416-4989-90a6-3cfe972175de',
   OPENSDER_API_KEY: '064ace11-a2a3-4dbd-9d97-fbc978ca29c3',
   OPS_API_KEY: 'testOpsApiKey',
-  MONGO_DB_URL: 'mongodb://127.0.0.1:5353/test'
+  MONGO_DB_URL: 'mongodb://127.0.0.1:5353/test',
+  DOC_LOGIN: 'login',
+  DOC_PASSWORD: 'password'
 }
