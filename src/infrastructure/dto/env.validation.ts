71ad0b84efbf7425b713c299880128ff386893f9
Armen Ozcelik <armen.ozcelik@octo.com>
2023-07-05 10:01:09 +0200

    tech: move env.validation into infrastructure folder

--- src/infrastructure/dto/env.validation.ts
+++ src/infrastructure/dto/env.validation.ts
@@ -1,22 +1,22 @@
-import * as Joi from 'joi';
+import * as Joi from 'joi'
 
 export const envValidationConfig = {
   cache: true,
   validationSchema: Joi.object({
     LABEL_API_KEY: Joi.string().guid({
       version: ['uuidv4'],
-      separator: true,
+      separator: true
     }),
     NORMALIZATION_API_KEY: Joi.string().guid({
       version: ['uuidv4'],
-      separator: true,
+      separator: true
     }),
     OPENSDER_API_KEY: Joi.string().guid({
       version: ['uuidv4'],
-      separator: true,
+      separator: true
     }),
     DOC_LOGIN: Joi.string().required(),
     DOC_PASSWORD: Joi.string().required(),
-    MONGO_DB_URL: Joi.string().required(),
-  }),
-};
+    MONGO_DB_URL: Joi.string().required()
+  })
+}
