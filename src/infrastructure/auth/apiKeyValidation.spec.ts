0136adf306bc87807d422d42d060e45f1e231497
Armen Ozcelik <armen.ozcelik@octo.com>
2023-07-21 16:44:47 +0200

    REVIEW

--- src/infrastructure/auth/apiKeyValidation.spec.ts
+++ src/infrastructure/auth/apiKeyValidation.spec.ts
@@ -1,19 +1,13 @@
 import { ApiKeyValidation } from './apiKeyValidation'
 
 describe('ApiKeyValidation', () => {
-  let apiKeyValidation: ApiKeyValidation
-
-  beforeAll(async () => {
-    apiKeyValidation = new ApiKeyValidation()
-  })
-
   it('returns false when provided API Key is not in valid API Key list', async () => {
     // GIVEN
     const apiKey = 'some-api-key'
     const authorizedApiKeys = ['some', 'valid', 'api', 'key']
 
     // WHEN
-    const res = apiKeyValidation.isValidApiKey(authorizedApiKeys, apiKey)
+    const res = ApiKeyValidation.isValidApiKey(authorizedApiKeys, apiKey)
 
     // THEN
     expect(res).toBe(false)
@@ -25,7 +19,7 @@ describe('ApiKeyValidation', () => {
     const authorizedApiKeys = ['some', 'valid', 'api', 'key', 'some-api-key']
 
     // WHEN
-    const res = apiKeyValidation.isValidApiKey(authorizedApiKeys, apiKey)
+    const res = ApiKeyValidation.isValidApiKey(authorizedApiKeys, apiKey)
 
     // THEN
     expect(res).toBe(true)
