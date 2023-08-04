f128593a86f3e6aae16b0d4db06fa2cdc592bf32
ArmenOzcelik <112550415+ArmenOzcelik@users.noreply.github.com>
2023-08-04 11:31:22 +0200

    feat(459&499): update Decision Statuses

--- src/domain/enum.ts
+++ src/domain/enum.ts
@@ -1,12 +1,14 @@
 export enum DecisionStatus {
-  TO_BE_TREATED = 'toBeTreated',
+  TOBETREATED = 'toBeTreated',
   LOADED = 'loaded',
   DONE = 'done',
   EXPORTED = 'exported',
   BLOCKED = 'blocked',
-  TOIGNORE = 'toIgnore',
-  IGNORED_DECISIONNONPUBLIQUE = 'ignored_decisionNonPublique',
-  IGNORED_DATEDECISIONINCOHERENTE = 'ignored_dateDecisionIncoherente'
+  IGNORED_DECISION_NON_PUBLIQUE = 'ignored_decisionNonPublique',
+  IGNORED_DATE_DECISION_INCOHERENTE = 'ignored_dateDecisionIncoherente',
+  IGNORED_CODE_NAC_NON_TRANSMIS_CC = 'ignored_codeNACnonTransmisCC',
+  IGNORED_CODE_NAC_DECISION_NON_PUBLIQUE = 'ignored_codeNACdeDecisionNonPublique',
+  IGNORED_CODE_NAC_DECISION_PARTIELLEMENT_PUBLIQUE = 'ignored_codeNACdeDecisionPartiellementPublique'
 }
 
 export enum Sources {
