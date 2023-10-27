bf448ac58d96490140e3151d88437c37c2caf882
Fabricio ALEGRIA <80031577+ahfabricio@users.noreply.github.com>
2023-10-27 09:31:39 +0200

    Tech (562): doc responses api

--- src/infrastructure/controllers/responses/createDecision.response.ts
+++ src/infrastructure/controllers/responses/createDecision.response.ts
@@ -1,4 +1,17 @@
-export interface CreateDecisionResponse {
+import { ApiProperty } from '@nestjs/swagger'
+import { MockUtils } from '../../utils/mock.utils'
+
+const mockutils = new MockUtils()
+export class CreateDecisionResponse {
+  @ApiProperty({
+    description: 'Identifiant de la décision créée',
+    example: mockutils.createDecisionResponse._id
+  })
   _id: string
+
+  @ApiProperty({
+    description: 'Réponse de la création de la décision',
+    example: mockutils.createDecisionResponse.message
+  })
   message: string
 }
--- src/infrastructure/controllers/responses/getDecisionsList.response.ts
+++ src/infrastructure/controllers/responses/getDecisionsList.response.ts
@@ -1,8 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { LabelStatus, Sources } from 'dbsder-api-types'
+import { MockUtils } from '../../utils/mock.utils'
 
-export interface GetDecisionsListResponse {
+const mockUtils = new MockUtils()
+export class GetDecisionsListResponse {
+  @ApiProperty({
+    description: 'Identifiant de la décision',
+    example: mockUtils.decisionTJToBeTreated._id
+  })
   _id: string
+  @ApiProperty({
+    description: 'Statut de la décision',
+    example: mockUtils.decisionTJToBeTreated.status
+  })
   status: LabelStatus
+  @ApiProperty({
+    description: 'Source de la décision',
+    example: mockUtils.decisionTJToBeTreated.source
+  })
   source: Sources
+  @ApiProperty({
+    description: 'Date de création de la décision au format ISO 8601',
+    example: mockUtils.decisionTJToBeTreated.dateCreation
+  })
   dateCreation: string
 }
