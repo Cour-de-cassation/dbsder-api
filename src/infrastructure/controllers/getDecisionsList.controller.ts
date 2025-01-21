c12a1e1c51eb3e039dbf0a2362bbaa1aea19caa0
Antoine Jeanneney <antoine.jeanneney@justice.fr>
2025-01-21 15:07:56 +0100

    fix getDecisionList mandatory fileds on swagger

--- src/infrastructure/controllers/getDecisionsList.controller.ts
+++ src/infrastructure/controllers/getDecisionsList.controller.ts
@@ -3,11 +3,9 @@ import {
   ApiBadRequestResponse,
   ApiHeader,
   ApiOkResponse,
-  ApiQuery,
   ApiTags,
   ApiUnauthorizedResponse
 } from '@nestjs/swagger'
-import { LabelStatus } from 'dbsder-api-types'
 import { ApiKeyValidation } from '../auth/apiKeyValidation'
 import { DatabaseError } from '../../domain/errors/database.error'
 import { DecisionSearchCriteria } from '../../domain/decisionSearchCriteria'
@@ -33,23 +31,6 @@ export class ListDecisionsController {
   @ApiHeader({
     name: 'x-api-key',
     description: 'Clé API'
-  })
-  @ApiQuery({
-    name: 'status',
-    description: 'Décision intègre au format wordperfect et metadonnées associées.',
-    enum: LabelStatus
-  })
-  @ApiQuery({
-    name: 'dateType',
-    description: 'Date de la décision ou de création'
-  })
-  @ApiQuery({
-    name: 'startDate',
-    description: 'date de début de la période de recherche'
-  })
-  @ApiQuery({
-    name: 'endDate',
-    description: 'date de fin de la période de recherche'
   })
   @ApiOkResponse({ description: 'Une liste de décisions', type: [GetDecisionsListResponse] })
   @ApiBadRequestResponse({
