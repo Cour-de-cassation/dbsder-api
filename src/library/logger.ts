dd2775e506594acb80afeee8455e5b8274bce8a5
Julien Grach <julien.grach@justice.fr>
2025-04-23 12:24:26 +0200

    add linter and fix any

--- src/library/logger.ts
+++ src/library/logger.ts
@@ -1,43 +1,33 @@
-import pino, { LoggerOptions } from "pino";
-import pinoHttp from "pino-http";
+import pino, { LoggerOptions } from 'pino'
+import pinoHttp from 'pino-http'
 
 const pinoPrettyConf = {
-  target: "pino-pretty",
+  target: 'pino-pretty',
   options: {
     singleLine: true,
     colorize: true,
-    translateTime: "UTC:dd-mm-yyyy - HH:MM:ss Z",
-  },
-};
+    translateTime: 'UTC:dd-mm-yyyy - HH:MM:ss Z'
+  }
+}
 
 const loggerOptions: LoggerOptions = {
-  base: { appName: "dbsder-api-reloaded" },
+  base: { appName: 'dbsder-api-reloaded' },
   formatters: {
     level: (label) => {
       return {
-        logLevel: label.toUpperCase(),
-      };
-    },
+        logLevel: label.toUpperCase()
+      }
+    }
   },
   timestamp: () => `,"timestamp":"${new Date(Date.now()).toISOString()}"`,
   redact: {
-    paths: [
-      "req",
-      "res",
-      "headers",
-      "ip",
-      "responseTime",
-      "hostname",
-      "pid",
-      "level",
-    ],
-    censor: "",
-    remove: true,
+    paths: ['req', 'res', 'headers', 'ip', 'responseTime', 'hostname', 'pid', 'level'],
+    censor: '',
+    remove: true
   },
-  transport:
-    process.env.NODE_ENV === "development" ? pinoPrettyConf : undefined,
-};
+  transport: process.env.NODE_ENV === 'development' ? pinoPrettyConf : undefined
+}
 
-export const logger = pino(loggerOptions);
+export const logger = pino(loggerOptions)
 
-export const loggerHttp = pinoHttp(loggerOptions); // attach logger instance at req (req.log will log message and req info)
+export const loggerHttp = pinoHttp(loggerOptions) // attach logger instance at req (req.log will log message and req info)
