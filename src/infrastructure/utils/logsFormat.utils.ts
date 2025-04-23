f1017d76250d0ae4c6d679f7a3a42b2a064a14f7
Julien Grach <julien.grach@justice.fr>
2025-04-23 10:39:18 +0200

    remove old documentation

--- src/infrastructure/utils/logsFormat.utils.ts
+++
@@ -1,8 +0,0 @@
-export class LogsFormat {
-  operationName: string
-  msg: string
-  data?: any
-  httpMethod?: string
-  path?: string
-  statusCode?: number
-}
--- src/usecase/updateStatut.usecase.ts
+++
@@ -1,9 +0,0 @@
-import { InterfaceDecisionsRepository } from '../domain/decisions.repository.interface'
-
-export class UpdateStatutUsecase {
-  constructor(private decisionsRepository: InterfaceDecisionsRepository) {}
-
-  async execute(decisionId: string, decisionStatus: string): Promise<string> {
-    return this.decisionsRepository.updateStatut(decisionId, decisionStatus)
-  }
-}
