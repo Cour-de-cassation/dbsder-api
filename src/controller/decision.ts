0c8f9760818c8f162b076cf51ed74cb38b25f307
Julien Grach <julien.grach@justice.fr>
2025-04-23 12:24:26 +0200

    add linter and fix any

--- src/controller/decision.ts
+++ src/controller/decision.ts
@@ -1,92 +1,88 @@
-import { Request, Router } from "express";
+import { Request, Router } from 'express'
 import {
   parseDecisionListFilters,
   parseId,
   parseUnIdentifiedDecisionSupported,
   parseUpdatableDecisionFields,
   UnIdentifiedDecisionSupported,
-  UpdatableDecisionFields,
-} from "../service/decision/models";
+  UpdatableDecisionFields
+} from '../service/decision/models'
 import {
   fetchDecisionById,
   fetchDecisions,
   saveDecision,
-  updateDecision,
-} from "../service/decision/handler";
-import { forbiddenError, missingValue } from "../library/error";
-import { Service } from "../service/authentication";
+  updateDecision
+} from '../service/decision/handler'
+import { forbiddenError, missingValue } from '../library/error'
+import { Service } from '../service/authentication'
 
-const app = Router();
+const app = Router()
 
-app.get("/decisions/:id", async (req, res, next) => {
+app.get('/decisions/:id', async (req, res, next) => {
   try {
-    const decisionId = parseId(req.params.id);
-    const decision = await fetchDecisionById(decisionId);
-    res.send(decision);
+    const decisionId = parseId(req.params.id)
+    const decision = await fetchDecisionById(decisionId)
+    res.send(decision)
   } catch (err: unknown) {
-    req.log.error(err);
-    next(err);
+    req.log.error(err)
+    next(err)
   }
-});
+})
 
-app.get("/decisions", async (req, res, next) => {
+app.get('/decisions', async (req, res, next) => {
   try {
-    const filters = parseDecisionListFilters(req.query);
-    const decision = await fetchDecisions(filters);
-    res.send(decision);
+    const filters = parseDecisionListFilters(req.query)
+    const decision = await fetchDecisions(filters)
+    res.send(decision)
   } catch (err: unknown) {
-    req.log.error(err);
-    next(err);
+    req.log.error(err)
+    next(err)
   }
-});
+})
 
-function parsePatchBody(body: Request["body"]): UpdatableDecisionFields {
-  if (!body)
-    throw missingValue("req.body", new Error(`body is missing on request`));
-  const maybeBody = JSON.parse(body);
-  return parseUpdatableDecisionFields(maybeBody);
+function parsePatchBody(body: Request['body']): UpdatableDecisionFields {
+  if (!body) throw missingValue('req.body', new Error('body is missing on request'))
+  const maybeBody = JSON.parse(body)
+  return parseUpdatableDecisionFields(maybeBody)
 }
 
-app.patch("/decisions/:id", async (req, res, next) => {
+app.patch('/decisions/:id', async (req, res, next) => {
   try {
-    if (req.context.service !== Service.LABEL)
-      throw forbiddenError(new Error());
+    if (req.context.service !== Service.LABEL) throw forbiddenError(new Error())
 
-    const id = parseId(req.params.id);
-    const updateFields = parsePatchBody(req.body);
-    const { _id } = await updateDecision(id, updateFields);
+    const id = parseId(req.params.id)
+    const updateFields = parsePatchBody(req.body)
+    const { _id } = await updateDecision(id, updateFields)
     res.send({
       _id,
-      message: "Decision mise à jour",
-    });
+      message: 'Decision mise à jour'
+    })
   } catch (err: unknown) {
-    req.log.error(err);
-    next(err);
+    req.log.error(err)
+    next(err)
   }
-});
+})
 
-function parsePutBody(body: Request["body"]): UnIdentifiedDecisionSupported {
-  if (!body)
-    throw missingValue("req.body", new Error(`body is missing on request`));
-  const maybeBody = JSON.parse(body);
-  return parseUnIdentifiedDecisionSupported(maybeBody);
+function parsePutBody(body: Request['body']): UnIdentifiedDecisionSupported {
+  if (!body) throw missingValue('req.body', new Error('body is missing on request'))
+  const maybeBody = JSON.parse(body)
+  return parseUnIdentifiedDecisionSupported(maybeBody)
 }
 
-app.put("/decisions", async (req, res, next) => {
+app.put('/decisions', async (req, res, next) => {
   try {
-    if (req.context.service !== Service.NORMALIZATION)
-      throw forbiddenError(new Error());
+    if (req.context.service !== Service.NORMALIZATION) throw forbiddenError(new Error())
 
-    const decision = parsePutBody(req.body);
-    const { _id } = await saveDecision(decision);
+    const decision = parsePutBody(req.body)
+    const { _id } = await saveDecision(decision)
     res.send({
       _id,
-      message: "Decision créée ou mise à jour",
-    });
+      message: 'Decision créée ou mise à jour'
+    })
   } catch (err: unknown) {
-    req.log.error(err);
-    next(err);
+    req.log.error(err)
+    next(err)
   }
-});
+})
 
-export default app;
+export default app
