fbacd8d92dd87071800344a64472a637483cb5ee
Julien Grach <julien.grach@justice.fr>
2025-04-23 12:24:26 +0200

    add linter and fix any

--- src/service/authentication.ts
+++ src/service/authentication.ts
@@ -1,19 +1,16 @@
-import { missingValue, unauthorizedError } from "../library/error";
+import { missingValue, unauthorizedError } from '../library/error'
 
-if (process.env.INDEX_API_KEY == null)
-  throw missingValue("process.env.INDEX_API_KEY", new Error());
-if (process.env.LABEL_API_KEY == null)
-  throw missingValue("process.env.LABEL_API_KEY", new Error());
+if (process.env.INDEX_API_KEY == null) throw missingValue('process.env.INDEX_API_KEY', new Error())
+if (process.env.LABEL_API_KEY == null) throw missingValue('process.env.LABEL_API_KEY', new Error())
 if (process.env.NORMALIZATION_API_KEY == null)
-  throw missingValue("process.env.NORMALIZATION_API_KEY", new Error());
+  throw missingValue('process.env.NORMALIZATION_API_KEY', new Error())
 if (process.env.OPENSDER_API_KEY == null)
-  throw missingValue("process.env.OPENSDER_API_KEY", new Error());
-if (process.env.OPS_API_KEY == null)
-  throw missingValue("process.env.OPS_API_KEY", new Error());
+  throw missingValue('process.env.OPENSDER_API_KEY', new Error())
+if (process.env.OPS_API_KEY == null) throw missingValue('process.env.OPS_API_KEY', new Error())
 if (process.env.PUBLICATION_API_KEY == null)
-  throw missingValue("process.env.PUBLICATION_API_KEY", new Error());
+  throw missingValue('process.env.PUBLICATION_API_KEY', new Error())
 if (process.env.ATTACHMENTS_API_KEY == null)
-  throw missingValue("process.env.ATTACHMENTS_API_KEY", new Error());
+  throw missingValue('process.env.ATTACHMENTS_API_KEY', new Error())
 
 const {
   INDEX_API_KEY,
@@ -22,37 +19,37 @@ const {
   OPENSDER_API_KEY,
   OPS_API_KEY,
   PUBLICATION_API_KEY,
-  ATTACHMENTS_API_KEY,
-} = process.env;
+  ATTACHMENTS_API_KEY
+} = process.env
 
 // Warn: I don't understand some of this services
 export enum Service {
-  INDEX = "index",
-  LABEL = "label",
-  NORMALIZATION = "normalization",
-  OPS = "ops",
-  PUBLICATION = "publication",
-  ATTACHEMENTS = "attachements",
-  OPENSDER = "opensder",
+  INDEX = 'index',
+  LABEL = 'label',
+  NORMALIZATION = 'normalization',
+  OPS = 'ops',
+  PUBLICATION = 'publication',
+  ATTACHEMENTS = 'attachements',
+  OPENSDER = 'opensder'
 }
 
 export function apiKeyToService(apiKey: string): Service {
   switch (apiKey) {
     case INDEX_API_KEY:
-      return Service.INDEX;
+      return Service.INDEX
     case LABEL_API_KEY:
-      return Service.LABEL;
+      return Service.LABEL
     case NORMALIZATION_API_KEY:
-      return Service.NORMALIZATION;
+      return Service.NORMALIZATION
     case OPS_API_KEY:
-      return Service.OPS;
+      return Service.OPS
     case OPENSDER_API_KEY:
-      return Service.OPENSDER;
+      return Service.OPENSDER
     case PUBLICATION_API_KEY:
-      return Service.PUBLICATION;
+      return Service.PUBLICATION
     case ATTACHMENTS_API_KEY:
-      return Service.ATTACHEMENTS;
+      return Service.ATTACHEMENTS
     default:
-      throw unauthorizedError(new Error());
+      throw unauthorizedError(new Error())
   }
 }
