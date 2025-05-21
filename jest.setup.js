9dd7343f90e90f9ad01b347c0078af930fe67b35
Julien Grach <julien.grach@justice.fr>
2025-05-21 11:52:49 +0200

    setup env to jest test

---
+++ jest.setup.js
@@ -0,0 +1,14 @@
+process.env = {
+  ...process.env,
+  ATTACHMENTS_API_KEY: '',
+  INDEX_API_KEY: '',
+  LABEL_API_KEY: '',
+  MONGO_DB_URL: 'mongodb://useless', // set to pass scheme
+  NODE_ENV: 'test',
+  NORMALIZATION_API_KEY: '',
+  OPENSDER_API_KEY: '',
+  OPS_API_KEY: '',
+  PORT: '',
+  PUBLICATION_API_KEY: '',
+  ZONING_API_URL: ''
+}
