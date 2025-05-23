e068038f618eecef92555130828749d360cf4760
Antoine Jeanneney <antoine.jeanneney@justice.fr>
2025-05-23 10:15:24 +0200

    get codeNac by codeNac instead of id

--- src/service/codeNac/handler.ts
+++ src/service/codeNac/handler.ts
@@ -2,8 +2,8 @@ import { CodeNac } from 'dbsder-api-types'
 import { notFound } from '../../library/error'
 import { findCodeNac } from '../../library/sderDB'
 
-export async function fetchCodeNacById(codeNacId: CodeNac['_id']): Promise<CodeNac> {
-  const codeNac = await findCodeNac({ _id: codeNacId })
-  if (!codeNac) throw notFound('codeNac', new Error())
-  return codeNac
+export async function fetchCodeNacByCodeNac(codeNac: CodeNac['codeNAC']): Promise<CodeNac> {
+  const codeNacDetails = await findCodeNac({ codeNAC: codeNac })
+  if (!codeNacDetails) throw notFound('codeNac', new Error())
+  return codeNacDetails
 }
