1d60ebbdde03968e3ebf7d016f56d19e0538114b
Julien Grach <julien.grach@justice.fr>
2025-05-27 15:42:18 +0200

    update dbsder-api-types

--- src/service/codeNac/models.ts
+++ src/service/codeNac/models.ts
@@ -1,8 +1,13 @@
-import { isId } from 'dbsder-api-types'
+import { parseId as parseDbsderId } from 'dbsder-api-types'
 import { notSupported } from '../../library/error'
 import { ObjectId } from 'mongodb'
 
 export function parseId(maybeId: unknown): ObjectId {
-  if (!isId(maybeId)) throw notSupported('id', maybeId, new Error('Given ID is not a valid ID'))
-  return new ObjectId(maybeId)
+  try {
+    return parseDbsderId(maybeId)
+  } catch (err) {
+    throw err instanceof Error
+      ? notSupported('id', maybeId, err)
+      : notSupported('id', maybeId, new Error('Given ID is not a valid ID'))
+  }
 }
