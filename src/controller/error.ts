fbbe4fcb98312e60b7b06418e260d508fb520134
juliengrach-cassation <julien.grach@justice.fr>
2025-09-24 17:45:53 +0200

    fix 503 caused by logger (#196)

--- src/controller/error.ts
+++ src/controller/error.ts
@@ -1,7 +1,9 @@
 import { NextFunction, Request, Response } from 'express'
 import { isCustomError } from '../library/error'
+import { responseLog } from './logger'
 
-export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+/* eslint-disable @typescript-eslint/no-unused-vars -- maybe we can remove */
+export const errorHandler = (err: Error, req: Request, res: Response, _: NextFunction) => {
   req.log.error({
     path: 'src/controller/error.ts',
     operations: ['other', `${req.method} ${req.path}`],
@@ -15,23 +17,23 @@ export const errorHandler = (err: Error, req: Request, res: Response, next: Next
       case 'missingValue':
         res.status(400)
         res.send({ error: err })
-        return next()
+        return responseLog(req, res)
       case 'notFound':
         res.status(404)
         res.send({ error: err })
-        return next()
+        return responseLog(req, res)
       case 'unauthorizedError':
         res.status(401)
         res.send({ error: err })
-        return next()
+        return responseLog(req, res)
       case 'forbiddenError':
         res.status(403)
         res.send({ error: err })
-        return next()
+        return responseLog(req, res)
     }
   }
 
   res.status(500)
   res.send({ error: 'Something wrong on server, please contact us' })
-  return next()
+  return responseLog(req, res)
 }
