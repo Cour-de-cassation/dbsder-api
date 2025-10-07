33e033f2b9cd1b5495c9597753b8d494e3b8efbe
YATERA Boubacar <boubacar.yatera@justice.fr>
2025-10-07 16:23:58 +0200

    remove console log and fix lint

--- src/service/affaire/models.ts
+++ src/service/affaire/models.ts
@@ -1,24 +1,24 @@
-import { Filter, ObjectId } from "mongodb"
-import { Affaire, ParseError } from "dbsder-api-types"
-import { toNotSupported } from "../../library/error";
+import { Filter, ObjectId } from 'mongodb'
+import { Affaire, ParseError } from 'dbsder-api-types'
+import { toNotSupported } from '../../library/error'
 
 export function buildAffaireFilter(filters: {
-    decisionId?: string;
-    numeroPourvoi?: string;
+  decisionId?: string
+  numeroPourvoi?: string
 }): Filter<Affaire> {
-    try {
-        const mongoFilter: Filter<Affaire> = {};
-        if (filters.decisionId) {
-            mongoFilter.decisionIds = { $in: [new ObjectId(filters.decisionId)] };
-        }
-
-        if (filters.numeroPourvoi) {
-            mongoFilter.numeroPourvois = { $in: [filters.numeroPourvoi] };
-        }
-
-        return mongoFilter;
-    } catch (error: unknown) {
-        if (error instanceof ParseError) throw toNotSupported('affaire', filters, error)
-        else throw error
+  try {
+    const mongoFilter: Filter<Affaire> = {}
+    if (filters.decisionId) {
+      mongoFilter.decisionIds = { $in: [new ObjectId(filters.decisionId)] }
     }
+
+    if (filters.numeroPourvoi) {
+      mongoFilter.numeroPourvois = { $in: [filters.numeroPourvoi] }
+    }
+
+    return mongoFilter
+  } catch (error: unknown) {
+    if (error instanceof ParseError) throw toNotSupported('affaire', filters, error)
+    else throw error
+  }
 }
