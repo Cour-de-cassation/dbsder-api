0093e6a8a6b18a125b0b2078fc262c2451ec9388
Antoine Jeanneney <antoine.jeanneney@justice.fr>
2026-04-13 10:01:09 +0200

    fix tests

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-  testEnvironment: "node",
+  testEnvironment: 'node',
   transform: {
-    "^.+\.tsx?$": ["ts-jest",{}],
+    '^.+\.tsx?$': ['ts-jest', {}]
   },
-  setupFiles: ["./jest.setup.js"],
-  testMatch: [
-    "<rootDir>/src/**/*.test.ts",
-  ],
-};
+  setupFiles: ['./jest.setup.js'],
+  testMatch: ['<rootDir>/src/**/*.test.ts'],
+  moduleNameMapper: {
+    '^@dbsder-api-types$': '<rootDir>/src/dbsder-api-types/index'
+  }
+}
