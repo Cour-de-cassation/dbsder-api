53310a949e00f594ba3ac05bed97c5b9f749cc56
Antoine Jeanneney <antoine.jeanneney@justice.fr>
2026-08-28 10:14:14 +0200

    juricav2 sourceId is a string cause original ObjectId pass throuh toHexString method on normalisation

--- src/dbsder-api-types/typeGuards/decisions_cav2.zod.ts
+++ src/dbsder-api-types/typeGuards/decisions_cav2.zod.ts
@@ -30,7 +30,7 @@ export type PartyCaV2 = z.infer<typeof zParty>
 
 export const decisionCaV2Schema = z.object({
   _id: zObjectId,
-  sourceId: zObjectId,
+  sourceId: z.string(),
   rawFileId: z.string().optional(),
   events: zEvents.optional(),
   sourceName: z.literal('juricav2'),
