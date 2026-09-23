2d58efd47229e2cd2f6a1224cb09847f8fa3e544
Julien Grach <julien.grach@justice.fr>
2026-09-23 16:46:30 +0200

    feat: add service in requestLog

--- src/api/logger.ts
+++ src/api/logger.ts
@@ -3,7 +3,10 @@ import { Request, Response, NextFunction } from 'express'
 export const requestLog = (req: Request, _: Response, next: NextFunction) => {
   req.log.info({
     path: 'src/controller/logger.ts',
-    operations: ['other', `${req.method} ${req.path} received`]
+    operations: [
+      'other',
+      `${req.method} ${req.path} received from service: ${req.context?.service ?? 'unknown'}`
+    ]
   })
   next()
 }
